fb52ddedaed72e8316bc38dc5a26f8105eec396b
Sheeeev66 <roi.hakim@gmail.com>
2021-11-02 22:27:26 +0200

    Update MekaWolfsFactory.sol

--- contracts/MekaWolfsFactory.sol
+++ contracts/MekaWolfsFactory.sol
@@ -20,8 +20,17 @@ contract MekaWolfsFactory is Ownable, ERC3664Transferable, ERC721 {
     event NewMekaWolfMinted(uint id);
 
     constructor() ERC3664("") ERC721("Meka Wolfs", "MKW") {
-        
+        // ERC3664._mint(attrId, _name, _symbol, _uri);
     } // use token id 0?
+
+    /**
+     * How minting will work:
+     * 
+     * Whenever you generate something so it creates a new attribute.
+     * Each token can't have the same attribute of the same Symbol.
+     * 
+     * 
+     */
 
     function _mintWolf(address to) internal {
         ERC721._safeMint(to, _tokenIds.current());
@@ -31,6 +40,18 @@ contract MekaWolfsFactory is Ownable, ERC3664Transferable, ERC721 {
 
     function _generateMetadata() private {
         
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC3664, ERC721)
+        returns (bool)
+    {
+        return
+            interfaceId == type(ERC3664Transferable).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
 }
